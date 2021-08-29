e3b2aaae327e93518f25475637c949343486e54e
One2Remember <distantquasar1994@gmail.com>
2021-08-29 13:56:47 -0500

    adding all helper classes to my_utils

--- my_modules/my_utils.py
+++ my_modules/my_utils.py
@@ -6,7 +6,15 @@ import termios
 import asyncio
 import time
 import random
-from speed import *
+from picar_4wd.pwm import PWM
+from picar_4wd.adc import ADC
+from picar_4wd.pin import Pin
+from picar_4wd.motor import Motor
+from picar_4wd.servo import Servo
+from picar_4wd.ultrasonic import Ultrasonic 
+from picar_4wd.speed import Speed
+from picar_4wd.filedb import FileDB  
+from picar_4wd.utils import *
 from constants import * # my constant values
 from threading import Thread
 
