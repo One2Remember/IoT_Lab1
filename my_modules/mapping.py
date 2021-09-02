42e0c6c672773e5bdc8c44ec262ef2720730c421
One2Remember <distantquasar1994@gmail.com>
2021-09-02 14:27:57 -0500

    renamed consts

--- my_modules/mapping.py
+++ my_modules/mapping.py
@@ -37,20 +37,20 @@ def update_car_position_in_environment():
     
 # initialize environment as empty room (walls as obstacles)
 def init_environment():
-    global environment, car_heading, car_location, ROOM_HEIGHT, ROOM_WIDTH, CAR_HEIGHT
+    global environment, car_heading, car_location, ROOM_HEIGHT_CM, ROOM_WIDTH_CM, CAR_HEIGHT_CM
     
     # init environment with all zeros
-    environment = np.zeros((ROOM_HEIGHT,ROOM_WIDTH))
+    environment = np.zeros((ROOM_HEIGHT_CM,ROOM_WIDTH_CM))
     # set 4 walls as being obstacles 
-    environment[0] = np.ones(ROOM_WIDTH)
-    environment[-1] = np.ones(ROOM_WIDTH)
-    environment[:,0] = np.ones(ROOM_HEIGHT)
-    environment[:,-1] = np.ones(ROOM_HEIGHT)
+    environment[0] = np.ones(ROOM_WIDTH_CM)
+    environment[-1] = np.ones(ROOM_WIDTH_CM)
+    environment[:,0] = np.ones(ROOM_HEIGHT_CM)
+    environment[:,-1] = np.ones(ROOM_HEIGHT_CM)
     
     # set car initial heading to 0
     car_heading = 0 
     # set car initial location to top of grid, middle position
-    car_location = np.array([CAR_HEIGHT, ROOM_WIDTH // 2])
+    car_location = np.array([CAR_HEIGHT_CM, ROOM_WIDTH_CM // 2])
     
     return
     
@@ -65,7 +65,7 @@ def print_environment():
 # update environment using the readings from a 180 deg scan from the US sensor
 # along with interpolation
 def update_environment(readings):
-    global environment, car_heading, car_location, ANGLES, ROOM_HEIGHT, ROOM_WIDTH
+    global environment, car_heading, car_location, ANGLES, ROOM_HEIGHT_CM, ROOM_WIDTH_CM
     
     # get true angle measurements of each sensor reading in range (0,359)
     true_angles = ((ANGLES * -1 + 90) + car_heading) % 360
@@ -80,8 +80,8 @@ def update_environment(readings):
     
     # filter out any obstacles detected out of bounds of our array 
     true_coords = true_coords[(true_coords[0] >= 0 and 
-    true_coords[0] < ROOM_HEIGHT and true_coords[1] >= 0 and 
-    true_coords[1] < ROOM_WIDTH)]
+    true_coords[0] < ROOM_HEIGHT_CM and true_coords[1] >= 0 and 
+    true_coords[1] < ROOM_WIDTH_CM)]
     
     # now, use interpolation to fill in any obstacles 
     for i in range(true_coords[0].size() - 1):
@@ -111,23 +111,23 @@ def update_environment(readings):
 
 # set the neighborhood of points around an obstacle as also being obstacles    
 def set_neighborhood_around_obstacle(x, y):
-    global environment, FUZZ_FACTOR, ROOM_HEIGHT, ROOM_WIDTH
+    global environment, FUZZ_FACTOR, ROOM_HEIGHT_CM, ROOM_WIDTH_CM
     round_x, round_y = round(x), round(y)
     
     x_s = np.arange(round_x - FUZZ_FACTOR, round_x + FUZZ_FACTOR + 1)
     y_s = np.arange(round_y - FUZZ_FACTOR, round_y + FUZZ_FACTOR + 1)
-    x_s = x_s[x_s >= 0 and x_s < ROOM_WIDTH]
-    y_s = y_s[y_s >=0 and y_s < ROOM_HEIGHT]
+    x_s = x_s[x_s >= 0 and x_s < ROOM_WIDTH_CM]
+    y_s = y_s[y_s >=0 and y_s < ROOM_HEIGHT_CM]
     
     environment[y_s,x_s] = 1
     
     
 # return whether a coordinate pair (y,x) is in room bounds    
 def coord_in_bounds(coord):
-    global ROOM_HEIGHT, ROOM_WIDTH
+    global ROOM_HEIGHT_CM, ROOM_WIDTH_CM
 
-    return (coord[0] >= 0 and coord[0] < ROOM_HEIGHT and coord[1] >= 0 and 
-    coord[1] < ROOM_WIDTH)
+    return (coord[0] >= 0 and coord[0] < ROOM_HEIGHT_CM and coord[1] >= 0 and 
+    coord[1] < ROOM_WIDTH_CM)
 
 # perform a 180 degree scan from the current location and heading
 # return np array of all readings at 15 degree intervals from -90 to 90
