05fff59af11a4925702ff2f82cdd26c817ea87b9
One2Remember <distantquasar1994@gmail.com>
2021-09-02 18:59:30 -0500

    testing mesh

--- my_modules/mapping.py
+++ my_modules/mapping.py
@@ -118,7 +118,7 @@ def update_environment(readings, angles=ANGLES):
         if coord_in_bounds(true_coords[i+1]):
             set_neighborhood_around_point(x_1,y_1)
         # interpolate the points between the two if both are valid 
-        if coord_in_bounds(true_coords[i]) and coord_in_bounds(true_coords[i+1]):
+        if coord_in_bounds(true_coords[i]) and coord_in_bounds(true_coords[i+1]) and x_1 != x_0:
             m = (y_1 - y_0) / (x_1 - x_0)
             b = y_0 - m * x_0
             # interpolate the points between them as well
