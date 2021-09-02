e26f2fad3eb1448b277ab6eba2a976deef349b48
One2Remember <distantquasar1994@gmail.com>
2021-09-02 15:39:54 -0500

    testing

--- my_modules/mapping.py
+++ my_modules/mapping.py
@@ -3,8 +3,10 @@ sys.path.append('./**/IoT_Lab1/picar_4wd/')
 import picar_4wd as fc
 from my_utils import *  # my utility methods
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-matplotlib.use('Agg') 
+
 from constants import * # my constant values
 
 # track environment as 300x290 np array using the following encoding scheme:
