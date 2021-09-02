4a4e9f737bcec858f87afc53f8afbcd62dc238e5
One2Remember <distantquasar1994@gmail.com>
2021-09-02 15:41:02 -0500

    testing

--- my_modules/mapping.py
+++ my_modules/mapping.py
@@ -157,6 +157,7 @@ def scan_angles(angles=ANGLES):
     for angle in angles:
         distance = fc.get_distance_at(angle)
         readings[i] = distance if distance > 0 and distance <= OBSTACLE_THRESHOLD else INF
+        delay(100)
         i += 1
         
     return readings
