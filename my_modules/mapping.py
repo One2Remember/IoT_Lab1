e8b2377b3c27ae37dadf3bfb934f23b31063d4ea
eg11 <eg11@illinois.edu>
2021-09-17 17:37:38 -0500

    tweaking tf params

--- my_modules/mapping.py
+++ my_modules/mapping.py
@@ -413,8 +413,8 @@ def main():
     num_scans = 0
     
     # start thread to detect things
-    tf_thread = Thread(target=run_object_detection, daemon=True)
-    tf_thread.start()
+    #tf_thread = Thread(target=run_object_detection, daemon=True)
+    #tf_thread.start()
     
     # main loop runs until we reach our goal 
     while distance_to(DESTINATION) > EPSILON:
@@ -423,7 +423,8 @@ def main():
         update_environment(readings, ANGLES)
         num_scans += 1
 
-        print_readings(readings)
+        # query tensorflow for a reading
+        capture_class(update_detections)
         
         # print environment
         print_graph_to_file(os.path.join(cur_time, "env_after_scan_" + str(num_scans)), environment, True)
