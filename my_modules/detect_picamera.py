5ea0bdcfe331b0e2a566b965aa0ba245f7c333c9
eg11 <eg11@illinois.edu>
2021-09-17 16:39:24 -0500

    tweaking tf params

--- my_modules/detect_picamera.py
+++ my_modules/detect_picamera.py
@@ -102,7 +102,7 @@ def detect_objects(interpreter, image, threshold):
 def capture_class(update_detections):
   default_labels = "files/coco_labels.txt"
   default_model = "files/detect.tflite"
-  default_threshold = 0.90
+  default_threshold = 1.00
 
   labels = load_labels(default_labels)
   label_names = np.array(labels)
